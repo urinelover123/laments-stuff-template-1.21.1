56b265a32afb47b2b4b113e6b4be340f8d3fd61d
jslament <bei.suyu.jia@gmail.com>
2025-03-16 16:42:26 +0100

    Initial commit

---
+++ src/main/java/net/jslament/lamentsstuff/LamentsStuff.java
@@ -0,0 +1,16 @@
+package net.jslament.lamentsstuff;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LamentsStuff implements ModInitializer {
+	public static final String MOD_ID = "laments-stuff";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/jslament/lamentsstuff/LamentsStuffClient.java
@@ -0,0 +1,10 @@
+package net.jslament.lamentsstuff;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LamentsStuffClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
